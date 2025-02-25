b7033577f72f165b881b2df08a26f50a9d5adc08
adriana-codes <helloadrianadiaz@gmail.com>
2025-02-25 12:27:01 +0100

    +Weather App set up
    App set up and integrated axios

---
+++ src/Weather.js
@@ -0,0 +1,17 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${Math.round(
+        response.data.main.temp
+      )}ºC`
+    );
+  }
+  let apiKey = "97f8e93f00107773f88eafd933ce86b7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return <p>Loading Weather for {props.city}...</p>;
+}
