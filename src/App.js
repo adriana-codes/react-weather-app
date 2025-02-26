c5261b2736c7c7c55c03207d716118860ea1f328
adriana-codes <helloadrianadiaz@gmail.com>
2025-02-26 13:05:46 +0100

    +Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,40 +4,42 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>React Weather App - Homework</h1>
-      </header>
-      <main>
-        {" "}
-        <Weather city="Barcelona" />
-        <br />
-      </main>
+      <div className="container">
+        <header>
+          <h1>React Weather App - Homework</h1>
+        </header>
+        <main>
+          {" "}
+          <Weather city="Barcelona" />
+          <br />
+        </main>
 
-      <footer>
-        <a
-          href="https://github.com/adriana-codes/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open sourced
-        </a>{" "}
-        project by{" "}
-        <a
-          href="https://github.com/adriana-codes"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Adri Díaz
-        </a>
-        👩🏻‍💻 hosted on{" "}
-        <a
-          href="https://adrianacodes-react-weather-app.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+        <footer>
+          <a
+            href="https://github.com/adriana-codes/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open sourced
+          </a>{" "}
+          project by{" "}
+          <a
+            href="https://github.com/adriana-codes"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Adri Díaz
+          </a>
+          👩🏻‍💻 hosted on{" "}
+          <a
+            href="https://adrianacodes-react-weather-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
