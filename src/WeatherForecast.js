feed7c1280f77a6ccda302d6e91597e5f1593f00
adriana-codes <helloadrianadiaz@gmail.com>
2025-03-06 11:05:54 +0100

    +Weather Forecast fix

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -9,12 +9,6 @@ export default function WeatherForecast(props) {
 
   useEffect(() => {
     setLoaded(false);
-    let longitude = props.coordinates.longitude;
-    let latitude = props.coordinates.latitude;
-    let apiKey = "0be192793f55aa475o5602t2cabd6e24";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
   }, [props.coordinates]);
 
   function handleResponse(response) {
@@ -28,17 +22,27 @@ export default function WeatherForecast(props) {
       <div>
         <hr />
         <div className="row mt-4">
-          {forecast.map((dailyForecast, index) =>
-            index < 5 ? (
-              <div className="col" key={index}>
-                <ForecastDay data={dailyForecast} />
-              </div>
-            ) : null
-          )}
+          {forecast.map((dailyForecast, index) => {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <ForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
     );
   } else {
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiKey = "0be192793f55aa475o5602t2cabd6e24";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
     return null;
   }
 }
